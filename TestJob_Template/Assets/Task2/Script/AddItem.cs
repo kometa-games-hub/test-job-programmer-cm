16f961741788715b9a1ce292aa193a4fc229cb6f
Pavel0Pivnov <58336243+Pavel0Pivnov@users.noreply.github.com>
2019-11-30 16:43:59 +0300

    Test

---
+++ TestJob_Template/Assets/Task2/Script/AddItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddItem : MonoBehaviour
+{
+    public GameObject Prefab;
+    public Transform ParentContent;
+
+    public void AddPref()
+    {
+        Instantiate(Prefab, Prefab.transform.position, Prefab.transform.rotation, ParentContent);
+    }
+}
